e8b3ad740aa77035afd02fc8ff905923972fd472
dmorello <daniel.morello@compsis.com.br>
2024-04-08 23:56:54 -0300

    nova rotina para envio do restart para  os slaves

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,57 @@
 #include "main.h"
+#include "Arduino.h"
+
+void printDataOnSerial()
+{
+  Serial.print("Origin: ");
+  Serial.println(message.origin);
+  Serial.print("MAC: ");
+  for (int i = 0; i < 6; i++)
+  {
+    Serial.printf("%02X:", message.macOrigin[i]);
+  }
+  Serial.println();
+  Serial.print("Micos: ");
+  Serial.println(message.timeMicros);
+  Serial.print("Temp: ");
+  Serial.println(message.coreTemp);
+  Serial.print("Type: ");
+  Serial.println(message.type);
+  Serial.print("Experiment: ");
+  Serial.println(message.experiment);
+  Serial.print("Position: ");
+  Serial.println(message.position);
+  Serial.println("=========================");
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  if (status == ESP_NOW_SEND_SUCCESS) {
+    Serial.println("Delivery Success");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(5);
+    digitalWrite(LED_BUILTIN, LOW);
+  } else
+  {
+    Serial.println("Delivery Fail");
+  }
+}
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&message, incomingData, sizeof(message));
-  queue.unshift(message);
-  JsonDocument doc;
-  deserializeJson(doc, message.json);
-  serializeJsonPretty(doc, Serial);
-  Serial.println();
+  printDataOnSerial();
 }
 
-void startExperiment() {
+void startExperiment()
+{
   digitalWrite(EXPERIMENT_PIN, HIGH);
   delay(highTime);
   digitalWrite(EXPERIMENT_PIN, LOW);
 }
- 
+
 void setup() {
+  pinMode(EXPERIMENT_PIN, OUTPUT);
+
   Serial.begin(115200);
   WiFi.mode(WIFI_AP_STA);
 
@@ -26,15 +62,49 @@ void setup() {
   }
   Serial.println("ESPNOW OK");
 
+  esp_now_register_send_cb(OnDataSent);
   esp_now_register_recv_cb(OnDataRecv);
-  pinMode(EXPERIMENT_PIN, OUTPUT);
+
+  for (size_t i = 0; i < numPeers; i++) {
+    memcpy(peerInfo.peer_addr, slaveAddresses[i], 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false; 
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+      Serial.println("Failed to add peer");
+      return;
+    }
+  }
+
 }
  
 void loop() {
-  delay(interval - highTime);
-  startExperiment();
-  interval = interval + pace;
-  if (interval > maxInterval) {
-    interval = minInterval;
+  delay(10000);
+  masterMessage.restart = true;
+  for (size_t i = 0; i < numPeers; i++) {
+    esp_err_t result = esp_now_send(slaveAddresses[i], (uint8_t *) &masterMessage, sizeof(masterMessage));
   }
+  delay(10000);
+
+  Serial.println("==================================");
+  Serial.println("=========START EXPERIMENT=========");
+  Serial.println("==================================");
+
+  while (interval <= maxInterval) {
+    delay(1000);
+    Serial.println("==================================");
+    Serial.print("=========INTERVAL ");
+    Serial.print(interval);
+    Serial.println("=============");
+    Serial.println("==================================");
+    startExperiment();
+    delay(interval - highTime);
+    startExperiment();
+    interval = interval + pace;
+  }
+  
+  Serial.println("==================================");
+  Serial.println("=========END EXPERIMENT=========");
+  Serial.println("==================================");
+
+  interval = 5000;
 }
--- src/main.h
+++ src/main.h
@@ -9,13 +9,38 @@
 unsigned long highTime = 600;
 unsigned long interval = 5000;
 unsigned long pace = 5000;
-unsigned long maxInterval = 120000;
+unsigned long maxInterval = 300000;
 unsigned long minInterval = 5000;
 
-typedef struct struct_message {
-  char json[200];
-} struct_message;
+const int numPeers = 4;
 
-struct_message message;
+uint8_t slaveAddresses[numPeers][6] = {
+  { 0xCC, 0x50, 0xE3, 0x96, 0x26, 0x08 },
+  { 0x24, 0x6F, 0x28, 0x79, 0xD7, 0xD8 },
+  { 0xAC, 0x67, 0xB2, 0x36, 0x1D, 0x64 },
+  { 0xAC, 0x67, 0xB2, 0x36, 0x1F, 0xDC }
+};
+esp_now_peer_info_t peerInfo;
 
-CircularBuffer<struct_message, 100> queue;
+typedef struct MasterMessage {
+  bool restart;
+} MasterMessage;
+
+MasterMessage masterMessage;
+
+typedef struct Message {
+  int origin;
+  uint8_t macOrigin[6];
+  unsigned long timeMicros;
+  float coreTemp;
+  int type;
+  unsigned long experiment;
+  int position;
+} Message;
+
+Message message;
+
+CircularBuffer<Message, 100> queue;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len);
